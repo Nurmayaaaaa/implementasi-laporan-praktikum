fd1a6e89dc530b76eb15de21326634255cd9f9bd
Nurmayaaaaa <240306054.mhs@uinmataram.ac.id>
2024-10-22 09:18:05 +0800

    Add files via upload

---
+++ perulangan while.py
@@ -0,0 +1,9 @@
+# memberikan nilai pada variabel b
+b= 20
+# kondisi perulangan while dimana nilai  lebih dari 10
+while b >= 10:   
+# mencetak nilai b      
+    print (b)
+# nilai b di kurangi dengan 2
+    b -= 2		
+# perulangan akan berhenti jika nilai b lebih kecil atau sama dengan 10
